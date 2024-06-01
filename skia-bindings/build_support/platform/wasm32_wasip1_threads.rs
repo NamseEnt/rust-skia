0cfde590f164b0353ee4df4c5ce8e7efea78c059
namse <skatpgusskat@naver.com>
2024-06-01 07:35:26 +0000

    Add path

--- skia-bindings/build_support/platform/wasm32_wasip1_threads.rs
+++ skia-bindings/build_support/platform/wasm32_wasip1_threads.rs
@@ -13,9 +13,12 @@ impl PlatformDetails for Wasm32Wasip1Threads {
         generic::gn_args(config, builder);
 
         builder
-            .arg("cc", quote("/opt/wasi-sdk/bin/clang"))
-            .arg("cxx", quote("/opt/wasi-sdk/bin/clang++"))
-            .arg("ar", quote("/opt/wasi-sdk/bin/llvm-ar"))
+            .arg("cc", quote(&format!("{}/bin/clang", wasi_sdk_base_dir())))
+            .arg(
+                "cxx",
+                quote(&format!("{}/bin/clang++", wasi_sdk_base_dir())),
+            )
+            .arg("ar", quote(&format!("{}/bin/llvm-ar", wasi_sdk_base_dir())))
             .arg("skia_gl_standard", quote("webgl"))
             .arg("skia_use_webgl", yes_if(features.gpu()))
             .arg("skia_use_freetype_woff2", yes())
@@ -26,57 +29,11 @@ impl PlatformDetails for Wasm32Wasip1Threads {
             // See https://github.com/rust-skia/rust-skia/issues/648
             .arg("skia_enable_fontmgr_custom_embedded", no())
             .arg("skia_enable_fontmgr_custom_empty", yes())
-            .cflags(
-                "
-                -DSK_BUILD_FOR_UNIX
-                -D__EMSCRIPTEN__
-
-                -mllvm
-                -wasm-enable-sjlj
-                -mtail-call
-                -D_WASI_EMULATED_MMAN
-                -pthread
-
-                -fvisibility=default
-
-                -Xclang -target-feature -Xclang +atomics
-                -Xclang -target-feature -Xclang +bulk-memory
-                -Xclang -target-feature -Xclang +mutable-globals
-
-                --sysroot=/opt/wasi-sdk/share/wasi-sysroot
-                -I/opt/wasi-sdk/lib/clang/18/include
-                -I/opt/emscripten/system/include
-                "
-                .split_whitespace()
-                .map(|s| s.to_string()),
-            );
+            .cflags(cflags());
     }
 
     fn bindgen_args(&self, _target: &Target, builder: &mut BindgenArgsBuilder) {
-        builder.args(
-            "
-            -DSK_BUILD_FOR_UNIX
-            -D__EMSCRIPTEN__
-
-            -mllvm
-            -wasm-enable-sjlj
-            -mtail-call
-            -D_WASI_EMULATED_MMAN
-            -pthread
-
-            -fvisibility=default
-
-            -Xclang -target-feature -Xclang +atomics
-            -Xclang -target-feature -Xclang +bulk-memory
-            -Xclang -target-feature -Xclang +mutable-globals
-
-            --sysroot=/opt/wasi-sdk/share/wasi-sysroot
-            -I/opt/wasi-sdk/lib/clang/18/include
-            -I/opt/emscripten/system/include
-            "
-            .split_whitespace()
-            .map(|s| s.to_string()),
-        );
+        builder.args(cflags());
     }
 
     fn link_libraries(&self, _features: &Features) -> Vec<String> {
@@ -103,5 +60,53 @@ impl PlatformDetails for Wasm32Wasip1Threads {
             // /opt/wasi-sdk/lib/clang/18/lib/wasip1/libclang_rt.builtins-wasm32.a
             format!("clang_rt.builtins-wasm32"),
         ]
+    }
+}
+
+fn cflags() -> Vec<String> {
+    format!(
+        "
+    -DSK_BUILD_FOR_UNIX
+    -D__EMSCRIPTEN__
+
+    -mllvm
+    -wasm-enable-sjlj
+    -mtail-call
+    -D_WASI_EMULATED_MMAN
+    -pthread
+
+    -fvisibility=default
+
+    -Xclang -target-feature -Xclang +atomics
+    -Xclang -target-feature -Xclang +bulk-memory
+    -Xclang -target-feature -Xclang +mutable-globals
+
+    --sysroot=/{wasi_sdk_base_dir}/share/wasi-sysroot
+    -I/{wasi_sdk_base_dir}/lib/clang/18/include
+    -I{emsdk_system_include}
+",
+        emsdk_system_include = emsdk_system_include(),
+        wasi_sdk_base_dir = wasi_sdk_base_dir(),
+    )
+    .split_whitespace()
+    .map(|s| s.to_string())
+    .collect()
+}
+
+fn emsdk_system_include() -> String {
+    match std::env::var("EMSDK_SYSTEM_INCLUDE") {
+        Ok(val) => val,
+        Err(_e) => panic!(
+            "please set the EMSDK_SYSTEM_INCLUDE environment variable to the {{emsdk}}/system/include directory"
+        ),
+    }
+}
+
+fn wasi_sdk_base_dir() -> String {
+    match std::env::var("WASI_SDK") {
+        Ok(val) => val,
+        Err(_e) => {
+            panic!("please set the WASI_SDK environment variable to the root of your wasi-sdk")
+        }
     }
 }
