88ea46bdca063ffe32987bf99244775d1605ea34
Nam Se Hyun <3580430+namse@users.noreply.github.com>
2025-05-26 10:37:34 +0900

    Updat env name

--- skia-bindings/build_support/binary_cache/download.rs
+++ skia-bindings/build_support/binary_cache/download.rs
@@ -173,7 +173,7 @@ fn should_try_download_binaries(
     config: &binaries_config::BinariesConfiguration,
     force: bool,
 ) -> Option<(String, String)> {
-    const RELEASE_COMMIT: &str = "866d11afa8252cd9b4d5";
+    const RELEASE_COMMIT: &str = "a7bec4944f61a547b799";
     const VERSION: &str = "0.84.0";
     match cargo::target().as_strs() {
         ("x86_64", "pc", "windows", Some("msvc")) => {
